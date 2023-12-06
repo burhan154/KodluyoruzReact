b8661e1c7db5abb039431cddeabbb3000c3bb633
Burhan Başaran <59660549+burhan154@users.noreply.github.com>
2023-12-06 17:21:41 +0300

    Add files via upload

---
+++ odev/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import GetData from './components/GetData';
+
+function App() {
+  GetData(1).then((result) => {
+    console.log(result);
+  })
+  
+  return (
+    <div>
+      <h1>User Data</h1>
+      <p>For User Data Open Console</p>
+    </div>
+  );
+};
+
+export default App;
