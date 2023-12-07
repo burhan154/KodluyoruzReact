6687c7a955d6c1d96d8696bcb760ef5bf6805cab
Burhan Başaran <59660549+burhan154@users.noreply.github.com>
2023-12-07 14:47:15 +0300

    Update GetData.js

--- odev/src/components/GetData.js
+++ odev/src/components/GetData.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 async function GetData(userId) {
   try {
+    //Gets user and post datas
     const user = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
     const posts = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-
+    
+    //Returns merged data
     return {...user.data, posts: posts.data}
   } catch (error) {
     return error.message;
