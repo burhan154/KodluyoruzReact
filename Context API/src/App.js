e95b29801d92f8470712bb89d20f02f751022c8e
Burhan Başaran <59660549+burhan154@users.noreply.github.com>
2023-12-25 09:41:06 +0300

    Add files via upload

---
+++ Context API/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Main from "./components/Main";
+import { MainProvider } from "./context/MainContext";
+
+function App() {
+  return (
+    <MainProvider>
+      <Main />
+    </MainProvider>
+  );
+}
+
+export default App;
